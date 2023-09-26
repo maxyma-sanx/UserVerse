26a035041a19004062bcb697be075d7aaa2345ca
maxyma-sanx <maxymasan.dev@gmail.com>
2023-09-26 14:08:20 +0300

    structure update

---
+++ src/components/ui/layout/bottomMenu/BottomMenu.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react';
+import {Text, View} from 'react-native';
+
+const BottomMenu: FC = () => {
+  return (
+    <View>
+      <Text>BottomMenu</Text>
+    </View>
+  );
+};
+
+export default BottomMenu;
---
+++ src/components/ui/layout/headerMenu/HeaderMenu.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react';
+import {Text, View} from 'react-native';
+
+const HeaderMenu: FC = () => {
+  return (
+    <View>
+      <Text>HeaderMenu</Text>
+    </View>
+  );
+};
+
+export default HeaderMenu;
