65fe5795dfe466349c5df34aef3ad8885a261a40
maxyma-sanx <maxymasan.dev@gmail.com>
2023-09-27 15:59:11 +0300

    fix overlay

--- src/ui/components/layout/overlay/Overlay.tsx
+++ src/ui/components/layout/overlay/Overlay.tsx
@@ -5,13 +5,15 @@ import {useAppSelector} from '../../../../redux/hooks';
 import {getUserLoading} from '../../../../redux/user/selector';
 import {getUsersLoading} from '../../../../redux/users/selector';
 import {getPostsLoading} from '../../../../redux/posts/selector';
+import {getPostLoading} from '../../../../redux/post/selector';
 
 const Container: FC = () => {
   const userLoading = useAppSelector(getUserLoading);
   const usersLoading = useAppSelector(getUsersLoading);
   const postsLoading = useAppSelector(getPostsLoading);
+  const postLoading = useAppSelector(getPostLoading);
 
-  const isLoading = userLoading || usersLoading || postsLoading;
+  const isLoading = userLoading || usersLoading || postsLoading || postLoading;
 
   return (
     <>
